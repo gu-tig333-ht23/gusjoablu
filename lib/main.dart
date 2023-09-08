643fcad339874e90b776162d9b5e3b3b20d49670
LudwigJL <ludwig.j.lundborg@gmail.com>
2023-09-08 12:13:26 +0200

    Vecka-1 app

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,126 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: MyHome());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Person {
+  final String name;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Person(this.name);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    List<Person> persons = [
+      Person('Meditate'),
+      Person('Write a book'),
+      Person('Have fun'),
+      Person('Nap'),
+      Person('Tell them to bring out the whole ocean!'),
+      Person('Tidy Room'),
+      Person('Wash dishes'),
+      Person('Study'),
+      Person("Tell em' to bring out the lobster"),
+      Person('Shop groceries'),
+      Person("Let's go golfing"),
+      Person('Walk the dog'),
+      Person("Let's go to the beach")
+    ];
+
     return Scaffold(
+      //Scafffold background color
+      backgroundColor: Color(0xFFFFFBFC),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        title: Column(
+          //Titeln är en bild hämtad från google.
+          children: [
+            Image.network(
+              'https://www.smokymountains.org/wp-content/uploads/sites/3/2021/03/Things-to-Do-txt-img.png',
+              height: 50,
+              width: 200,
             ),
           ],
         ),
+        backgroundColor: Color(0xFF285238),
+        centerTitle: true,
+        actions: [
+          // Skapar de tre prickarna till höger i appbar. Lägger till padding av 10p til höger om icon.
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(Icons.more_horiz),
+          ),
+        ],
       ),
+      // Floating button orange.
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {},
+        backgroundColor: Color(0xFFE88D67),
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      // Listview
+      body: ListView(
+        children: persons
+            .map((person) => Column(
+                  children: [
+                    _item(person.name),
+                    Divider(
+                      color: Colors.grey,
+                      thickness: 1.0,
+                    ),
+                  ],
+                ))
+            .toList(),
+      ),
+    );
+  }
+
+  Widget _item(String name) {
+    // Skapar utrymme för varje row (top och bottom).
+    return Padding(
+      padding: EdgeInsets.only(top: 10, bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Padding höger och vänster om icon checkbox.
+          Padding(
+            padding: EdgeInsets.only(right: 10, left: 10),
+            child: Icon(
+              Icons.check_box_outline_blank,
+              color: const Color(0xFFE88D67),
+            ),
+          ),
+          // Expanded
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(fontSize: 30),
+                ),
+              ],
+            ),
+          ),
+          // Close icon till höger. Padding 10p right, left.
+          Padding(
+              padding: EdgeInsets.only(right: 10, left: 10),
+              child: Icon(Icons.close)),
+        ],
+      ),
     );
   }
 }
